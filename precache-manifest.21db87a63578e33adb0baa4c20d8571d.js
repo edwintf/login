25145b38f64d60c2a110572bbaca92a481e09205
edwintf <edwintubac.f@outlook.com>
2019-09-09 02:16:34 -0600

    Updates

---
+++ precache-manifest.21db87a63578e33adb0baa4c20d8571d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981edebec026998f425825845fab1331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a80a6b64e28039cf95",
+    "url": "/static/css/main.66a115a2.chunk.css"
+  },
+  {
+    "revision": "fc826fb0933bb4dc2d49",
+    "url": "/static/js/2.f1c0885b.chunk.js"
+  },
+  {
+    "revision": "87a80a6b64e28039cf95",
+    "url": "/static/js/main.32433d86.chunk.js"
+  },
+  {
+    "revision": "48a01fa0bddb02527395",
+    "url": "/static/js/runtime~main.71b63140.js"
+  }
+]);
